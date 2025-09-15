aa51aa519fb90fabdc702db14a1ddfdd2fccb833
jx-dohwan <jx7789@gmail.com>
2025-09-15 22:08:29 +0900

    [FEAT] added jwt config

--- src/core/cache/cache.interface.ts
+++ src/core/cache/cache.interface.ts
@@ -4,6 +4,9 @@ export const CacheServiceKey = Symbol('CacheServiceKey');
 export const RedisClientKey = Symbol('RedisClientKey');
 
 export const CacheKeys = {
+  RefreshToken: 'refresh-token/',
+  TokenBlacklist: 'token-blacklist/',
+  User: 'user/',
   UserRank: 'user-rank/',
 } as const;
 export type CacheKeys = Union<typeof CacheKeys>;
--- src/core/config/config.service.ts
+++ src/core/config/config.service.ts
@@ -1,12 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import {
-    AppConfig,
-    Configurations,
-    DBConfig,
-    MongoDBConfig,
-    RedisConfig,
-  } from '.';
+  AppConfig,
+  Configurations,
+  DBConfig,
+  JwtConfig,
+  MongoDBConfig,
+  RedisConfig,
+} from '.';
 
 @Injectable()
 export class MoinConfigService {
@@ -19,12 +20,16 @@ export class MoinConfigService {
   getDBConfig(): DBConfig {
     return this.configService.getOrThrow('DB');
   }
-  
+
   getMongoDBConfig(): MongoDBConfig {
     return this.configService.getOrThrow('MONGODB');
   }
 
   getRedisConfig(): RedisConfig {
     return this.configService.getOrThrow('REDIS');
+  }
+
+  getJwtConfig(): JwtConfig {
+    return this.configService.getOrThrow('JWT');
   }
 }
